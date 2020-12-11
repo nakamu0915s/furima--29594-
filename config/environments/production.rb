e19b3a6b5ef25b1d4909ef0a8f4601dbd46381f9
nakamu0915s <mutagaeru0915@yahoo.co.jp>
2020-12-11 14:40:08 +0900

    名前の変更

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -58,7 +58,7 @@ Rails.application.configure do
 
   # Use a real queuing backend for Active Job (and separate queues per environment).
   # config.active_job.queue_adapter     = :resque
-  # config.active_job.queue_name_prefix = "furima_”29594”_production"
+  # config.active_job.queue_name_prefix = "furima_29594_production"
 
   config.action_mailer.perform_caching = false
 
